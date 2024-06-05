62fc6e73a6358ef18e1ba85711c9463b1abc1aa2
Adilbek-byte <70812093+Adilbek-byte@users.noreply.github.com>
2024-06-05 13:09:19 +0600

    Add files via upload

---
+++ SpotiPie.Domain/Repositories/ITrackRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SpotiPie.Domain.Repositories;
+
+public interface ITrackRepository : IRepository<Track>
+{
+    Task<Track?> GetByIdWithGenres(int id);
+    Task<Track?> GetByIdWithTrackData(int id);
+    Task<List<Track>> GetAllOfGenreAsync(string genre);
+    Task<List<Track>> GetAllOfArtistAsync(int artistId);
+}
