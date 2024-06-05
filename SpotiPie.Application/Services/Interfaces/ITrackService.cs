3d8eae4dc1a9f76ce41c050cbc221c517e3b3ed1
Adilbek-byte <70812093+Adilbek-byte@users.noreply.github.com>
2024-06-05 12:58:15 +0600

    Add files via upload

---
+++ SpotiPie.Application/Services/Interfaces/ITrackService.cs
@@ -0,0 +1,16 @@
+﻿using SpotiPie.Domain.Result;
+
+namespace SpotiPie.Application.Services.Interfaces;
+
+public interface ITrackService
+{
+    public Task<TrackGetDto?> GetByIdAsync(int id);
+    public Task<List<TrackGetDto>> GetAllAsync();
+    public Task<List<TrackGetDto>> GetByArtistAsync(int artistId);
+    public Task<TrackGetDto> CreateAsync(TrackCreateDto trackDto);
+    public Task<Result<TrackGetDto>> UpdateAsync(int id, TrackCreateDto trackDto);
+    public Task DeleteAsync(int id);
+    public Task<bool> AddGenreAsync(int id, int genreId);
+    public Task<bool> UploadDataAsync(TrackDataDto trackDataDto);
+    public Task<TrackDataDto?> DownloadDataAsync(int id);
+}
