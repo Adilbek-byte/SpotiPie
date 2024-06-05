e490fdf76d6841106737ec7555ba58aeead52210
Adilbek-byte <70812093+Adilbek-byte@users.noreply.github.com>
2024-06-05 13:19:15 +0600

    Add files via upload

---
+++ SpotiPie.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace SpotiPie.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> opts) : DbContext(opts)
+{
+    public DbSet<Artist> Artists => Set<Artist>();
+    public DbSet<Album> Albums => Set<Album>();
+    public DbSet<Track> Tracks => Set<Track>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Lyrics> Texts => Set<Lyrics>();
+    public DbSet<Genre> Genres => Set<Genre>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
